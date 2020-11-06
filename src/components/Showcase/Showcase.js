f9914b56c867a557299baa57e79f177d318eda98
Rodrigo Garbulha <rodrigo.garbulha@gmail.com>
2020-11-05 22:15:09 -0500

    content layout added

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -3,9 +3,26 @@ import React from 'react'
 import './showcase.css'
 
 export default function Showcase() {
+
+    var list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+
     return (
         <div className="display">
-            Showcase
+            <div><h3>20 Results</h3></div>
+            <div className="showcaseList">
+                {list.map(item=>
+                    (
+                        <div className="showcaseItem">
+                            <div className="showcaseZoom"></div>
+                            <div className="showcaseImage">Image</div>
+                            <div className="showcaseTopNote">New</div>
+                            <div className="showcaseTitle"><span>Male Jeans</span></div>
+                            <div className="showcasePrice"><span>$29.95</span></div>
+                            <div className="showcaseColours"><small>3 COLOURS</small></div>
+                        </div>
+                    )
+                )}
+            </div>
         </div>
     )
 }
--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
 
-import './sidenav.css'
+import "./sidenav.css";
 
 export default function SideNav() {
-    return (
-        <div className="sidemenu">
-            SideNav
-        </div>
-    )
+  return (
+    <div className="sidemenu">
+      SideNav
+      <div>
+      <select name="orderBy" id="orderBy">
+        <option value="latest">Newly Added</option>
+        <option value="low">Price: Low to High</option>
+        <option value="high">Price: High to Low</option>
+      </select>
+      </div>
+      <div>
+          <button>Reset</button>
+      </div>
+      <div>
+        <h4> Category </h4>
+        <ul>
+          <li>Pants</li>
+          <ul>
+            <li>Men Jeans</li>
+            <li>Women Jeans</li>
+          </ul>
+        </ul>
+      </div>
+      <div>
+          <h4>Price</h4>
+          <div style={{display:"flex",flexDirection:"column"}}>
+          <label htmlFor="priceFilter0"><input type="checkbox" name="priceFilter" id="priceFilter0" value="20-40"/>$20 - $40</label>
+          <label htmlFor="priceFilter1"><input type="checkbox" name="priceFilter" id="priceFilter1" value="40-60"/>$40 - $60</label>
+          <label htmlFor="priceFilter2"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>$60 - $80</label>
+          </div>
+      </div>
+      <div>
+          <h4>Color</h4>
+          <div style={{display:"flex",flexDirection:"row"}}>
+              <div id="black" style={{width:50,height:50,backgroundColor:"#000",borderRadius:25}}></div>
+              <div id="blue"  style={{width:50,height:50,backgroundColor:"#00f",borderRadius:25}}></div>
+          </div>
+      </div>
+      <div>
+          <h4>Size</h4>
+          <div>
+          <label htmlFor="sizeFilter0"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>XXS</label>
+          <label htmlFor="sizeFilter0"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>XS</label>
+          <label htmlFor="sizeFilter0"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>S</label>
+          <label htmlFor="sizeFilter0"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>M</label>
+          <label htmlFor="sizeFilter0"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>L</label>
+          <label htmlFor="sizeFilter0"><input type="checkbox" name="priceFilter" id="priceFilter2" value="60-80"/>XL</label>
+          </div>
+      </div>
+    </div>
+  );
 }
