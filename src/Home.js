3acbafd306e3dc08ba5b80a214d147979149f891
rgarbulha <rodrigo.garbulha@gmail.com>
2020-11-09 18:36:59 -0500

    reset filters action fixed

--- src/Home.js
+++ src/Home.js
@@ -1,141 +1,140 @@
-import { useState, useEffect } from 'react'
-import _ from 'lodash'
+import { useState, useEffect } from "react";
+import _ from "lodash";
 
-import SideNav from './components/SideNav/SideNav'
-import Showcase from './components/Showcase/Showcase'
+import SideNav from "./components/SideNav/SideNav";
+import Showcase from "./components/Showcase/Showcase";
 
-import Container from 'react-bootstrap/Container'
+import Container from "react-bootstrap/Container";
 
-import data from './data.json';
+import data from "./data.json";
 
-import './styles/store.css'
+import "./styles/store.css";
 
-import { colorsFilterInitialState, pricesFilterInitialState } from './constants/filters'
+import {
+  colorsFilterInitialState,
+  pricesFilterInitialState,
+} from "./constants/filters";
 
 const filtersInitialState = {
-    pricesFilter: pricesFilterInitialState,
-    colorsFilter: colorsFilterInitialState,
-    sizesFilter: null,
-    orderBy: null
-}
+  pricesFilter: pricesFilterInitialState,
+  colorsFilter: colorsFilterInitialState,
+  sizesFilter: null,
+  orderBy: "Newly Added",
+};
 
 const Home = (props) => {
+  const [filters, setFilters] = useState({ ...filtersInitialState });
 
-    const [filters, setFilters] = useState({ ...filtersInitialState })
-
-    const resetFilters = (currentFilters) => {
-
-        setFilters({
-            ...currentFilters, orderBy: "Newly Added"
-        })
-
-        console.log(currentFilters)
-        currentFilters.orderBy = "Newly Added"
-
-        _.forEach(currentFilters.colorsFilter, color => {
-            color.active = false
-        })
-
-
-        getProducts(null)
-    }
-
-    const handleOrderFilterChange = (filters, value) => {
-        var newFilters = { ...filters, orderBy: value }
-        setFilters(newFilters)
-        getProducts(newFilters)
-    }
-
-    const handleColorFilterChange = (filters, color) => {
-        var newColors = [...filters.colorsFilter]
-        newColors[newColors.indexOf(color)].active = !color.active
-        var newFilters = { ...filters, colorsFilter: newColors }
-        setFilters(newFilters)
-        getProducts(newFilters)
-    }
-
-    const filtersActions = {
-        resetFilters: resetFilters,
-        handleOrderFilterChange: handleOrderFilterChange,
-        handleColorFilterChange: handleColorFilterChange
-    }
-
-
-    const [state, setState] = useState({
-        cart: [],
-        products: [],
-        menuState: props.menuState,
+  const resetFilters = (currentFilters) => {
+    setFilters({
+        ...currentFilters,
+        orderBy: "Newly Added"
     })
 
-    const orderBy = (prods, order) => {
+    // console.log("current", currentFilters)
+    currentFilters.orderBy="Newly Added"
 
-        var ordered = []
-        switch (order) {
-            case "Newly Added":
-                ordered = _.orderBy(prods, ['dataAdded'])
-                return ordered
-            case "Low to High":
-                ordered = _.orderBy(prods, ['price'], ['asc', 'desc'])
-                return ordered
-            case "High to Low":
-                ordered = _.orderBy(prods, ['price'], ['desc', 'asc'])
-                return ordered
-            default:
-                return prods
-        }
+    
 
+    _.forEach(currentFilters.colorsFilter, (color) => {
+      color.active = false;
+    });
+
+    getProducts(null);
+  };
+
+  const handleOrderFilterChange = (filters, value) => {
+    var newFilters = { ...filters, orderBy: value };
+    setFilters(newFilters);
+    getProducts(newFilters);
+  };
+
+  const handleColorFilterChange = (filters, color) => {
+    var newColors = [...filters.colorsFilter];
+    newColors[newColors.indexOf(color)].active = !color.active;
+    var newFilters = { ...filters, colorsFilter: newColors };
+    setFilters(newFilters);
+    getProducts(newFilters);
+  };
+
+  const filtersActions = {
+    resetFilters: resetFilters,
+    handleOrderFilterChange: handleOrderFilterChange,
+    handleColorFilterChange: handleColorFilterChange,
+  };
+
+  const [state, setState] = useState({
+    cart: [],
+    products: [],
+    menuState: props.menuState,
+  });
+
+  const orderBy = (prods, order) => {
+    var ordered = [];
+    console.log("aqui",order)
+    switch (order) {
+      case "Newly Added":
+          console.log("newly")
+        ordered = _.orderBy(prods, ["dataAdded"]);
+        return ordered;
+      case "Low to High":
+        ordered = _.orderBy(prods, ["price"], ["asc", "desc"]);
+        return ordered;
+      case "High to Low":
+        ordered = _.orderBy(prods, ["price"], ["desc", "asc"]);
+        return ordered;
+      default:
+        return prods;
+    }
+  };
+
+  const getProducts = (filters = null) => {
+    var filtered = data;
+
+    if (filters) {
+      var activeColors = _.filter(filters.colorsFilter, (color) => {
+        return color.active === true;
+      });
+
+      if (activeColors.length) {
+        filtered = _.filter(data, (product) => {
+          var include = false;
+          for (let c in activeColors) {
+            if (product.colors.includes(activeColors[c].value)) include = true;
+          }
+
+          return include;
+        });
+      }
+
+      var ordered = orderBy(filtered, filters.orderBy);
+
+      setState({ ...state, products: ordered });
+    } else {
+      var ordered = orderBy(filtered, "Newly Added");
+      setState({ ...state, products: ordered });
     }
 
-    const getProducts = (filters = null) => {
-        var filtered = data
+    // console.log(filtered)
+  };
+  //product import here
+  useEffect(() => {
+    getProducts();
+    return () => {
+      props.cart.closeMenu();
+    };
+  }, []);
 
-        if (filters) {
-            var activeColors = _.filter(filters.colorsFilter, color => {
-                return color.active === true
-            })
+  //filters here
 
-            if (activeColors.length) {
-                filtered = _.filter(data, product => {
-                    var include = false
-                    for (let c in activeColors) {
-                        if (product.colors.includes(activeColors[c].value))
-                            include = true
-                    }
+  return (
+    <Container>
+      <div className="store">
+        <SideNav filtersActions={filtersActions} filters={filters} />{" "}
+        <Showcase addToCart={props.cart.addToCart} products={state.products} />{" "}
+      </div>{" "}
+    </Container>
+  );
+};
 
-                    return include
-                })
-            }
-
-            var ordered = orderBy(filtered, filters.orderBy)
-
-            setState({ ...state, products: ordered })
-        } else {
-            var ordered = orderBy(filtered, "")
-            setState({ ...state, products: ordered })
-        }
-
-        // console.log(filtered)
-
-
-    }
-    //product import here
-    useEffect(() => {
-        getProducts()
-        return () => {
-            props.cart.closeMenu()
-        }
-    }, [])
-
-    //filters here
-
-    return (
-        <Container>
-            <div className="store">
-                <SideNav filtersActions={filtersActions} filters={filters} />
-                <Showcase addToCart={props.cart.addToCart} products={state.products} />
-            </div>
-        </Container>
-    );
-}
-
-export default Home
+export default Home;
--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -4,73 +4,165 @@ import "./sidenav.css";
 
 export default function SideNav(props) {
 
-  const [state, setState] = useState({
-    pricesFilter: props.filters.pricesFilter,
-    colorsFilter: props.filters.colorsFilter,
-    sizesFilter: props.filters.sizesFilter,
-    orderBy: props.filters.orderBy
-  })
-
   const resetFilters = () => {
-    props.filtersActions.handleOrderFilterChange(props.filters, "Newly Added")
-    props.filtersActions.resetFilters(props.filters)
-    setState({ ...state, orderBy: props.filters.orderBy })
-  }
+    props.filtersActions.resetFilters(props.filters);
+  };
 
   useEffect(() => {
-  }, [])
+    console.log(props.filters.orderBy)
+  }, []);
 
   return (
     <div className="sidemenu">
       <div className="sidemenu-section">
-        <select name="orderBy" id="orderBy" className="orderBy" value={state.orderBy} onChange={(e) => props.filtersActions.handleOrderFilterChange(props.filters, e.target.value)}>
-          <option value="Newly Added">Newly Added</option>
-          <option value="Low to High">Price: Low to High</option>
-          <option value="High to Low">Price: High to Low</option>
-        </select>
-      </div>
+        <select
+          name="orderBy"
+          id="orderBy"
+          className="orderBy"
+          value={props.filters.orderBy}
+          onChange={(e) =>
+            props.filtersActions.handleOrderFilterChange(
+              props.filters,
+              e.target.value
+            )
+          }
+        >
+          <option value="Newly Added"> Newly Added </option>{" "}
+          <option value="Low to High"> Price: Low to High </option>{" "}
+          <option value="High to Low"> Price: High to Low </option>{" "}
+        </select>{" "}
+      </div>{" "}
       <div className="sidemenu-section">
-        <button className="button" onClick={resetFilters}>Reset</button>
-      </div>
+        <button className="button" onClick={resetFilters}>
+          {" "}
+          Reset{" "}
+        </button>{" "}
+      </div>{" "}
       <div className="sidemenu-section">
-        <h4> Category </h4>
+        <h4> Category </h4>{" "}
         <div className="category-display">
           <ul className="category-list">
-            <li><button className="category-btn">Pants</button></li>
+            <li>
+              {" "}
+              <button className="category-btn"> Pants </button>
+            </li>
             <ul className="category-list">
-              <li><button className="category-btn">Men Jeans</button></li>
-              <li><button className="category-btn">Women Jeans</button></li>
-            </ul>
-          </ul>
-        </div>
-      </div>
+              <li>
+                {" "}
+                <button className="category-btn"> Men Jeans </button>
+              </li>
+              <li>
+                {" "}
+                <button className="category-btn"> Women Jeans </button>
+              </li>
+            </ul>{" "}
+          </ul>{" "}
+        </div>{" "}
+      </div>{" "}
       <div className="sidemenu-section">
-        <h4>Price</h4>
-        <div className='sidemenu-price-filter'>
-          {state.pricesFilter.map((filter, i) => (
-            <label htmlFor={"priceFilter" + i}><input type="radio" name="priceFilter" id={"priceFilter" + i} value={filter.value} />{" "}{filter.text}</label>
-          ))}
-        </div>
-      </div>
+        <h4> Price </h4>{" "}
+        <div className="sidemenu-price-filter">
+          {" "}
+          {props.filters.pricesFilter.map((filter, i) => (
+            <label htmlFor={"priceFilter" + i}>
+              {" "}
+              <input
+                type="radio"
+                name="priceFilter"
+                id={"priceFilter" + i}
+                value={filter.value}
+              />{" "}
+              {filter.text}
+            </label>
+          ))}{" "}
+        </div>{" "}
+      </div>{" "}
       <div className="sidemenu-section">
-        <h4>Color</h4>
+        <h4> Color </h4>{" "}
         <div className={"sidemenu-color-display"}>
-          {state.colorsFilter.map((color, i) => (
-            <div id={"colorFilter" + i} className={color.active === true ? "active-color-filter" : ""} style={{ backgroundColor: color.color }} onClick={() => props.filtersActions.handleColorFilterChange(props.filters, color)}></div>
-          ))}
-        </div>
-      </div>
+          {" "}
+          {props.filters.colorsFilter.map((color, i) => (
+            <div
+              id={"colorFilter" + i}
+              className={color.active === true ? "active-color-filter" : ""}
+              style={{ backgroundColor: color.color }}
+              onClick={() =>
+                props.filtersActions.handleColorFilterChange(
+                  props.filters,
+                  color
+                )
+              }
+            >
+              {" "}
+            </div>
+          ))}{" "}
+        </div>{" "}
+      </div>{" "}
       <div className="sidemenu-section">
-        <h4>Size</h4>
+        <h4> Size </h4>{" "}
         <div className="sidemenu-size-filter">
-          <label htmlFor="sizeFilter0"><input type="checkbox" name="sizeFilter" id="sizeFilter0" value="XXS" /> XXS</label>
-          <label htmlFor="sizeFilter1"><input type="checkbox" name="sizeFilter" id="sizeFilter1" value="XS" /> XS</label>
-          <label htmlFor="sizeFilter2"><input type="checkbox" name="sizeFilter" id="sizeFilter2" value="S" /> S</label>
-          <label htmlFor="sizeFilter3"><input type="checkbox" name="sizeFilter" id="sizeFilter3" value="M" /> M</label>
-          <label htmlFor="sizeFilter4"><input type="checkbox" name="sizeFilter" id="sizeFilter4" value="L" /> L</label>
-          <label htmlFor="sizeFilter5"><input type="checkbox" name="sizeFilter" id="sizeFilter5" value="XL" /> XL</label>
-        </div>
-      </div>
+          <label htmlFor="sizeFilter0">
+            {" "}
+            <input
+              type="checkbox"
+              name="sizeFilter"
+              id="sizeFilter0"
+              value="XXS"
+            />{" "}
+            XXS{" "}
+          </label>{" "}
+          <label htmlFor="sizeFilter1">
+            {" "}
+            <input
+              type="checkbox"
+              name="sizeFilter"
+              id="sizeFilter1"
+              value="XS"
+            />{" "}
+            XS{" "}
+          </label>{" "}
+          <label htmlFor="sizeFilter2">
+            {" "}
+            <input
+              type="checkbox"
+              name="sizeFilter"
+              id="sizeFilter2"
+              value="S"
+            />{" "}
+            S{" "}
+          </label>{" "}
+          <label htmlFor="sizeFilter3">
+            {" "}
+            <input
+              type="checkbox"
+              name="sizeFilter"
+              id="sizeFilter3"
+              value="M"
+            />{" "}
+            M{" "}
+          </label>{" "}
+          <label htmlFor="sizeFilter4">
+            {" "}
+            <input
+              type="checkbox"
+              name="sizeFilter"
+              id="sizeFilter4"
+              value="L"
+            />{" "}
+            L{" "}
+          </label>{" "}
+          <label htmlFor="sizeFilter5">
+            {" "}
+            <input
+              type="checkbox"
+              name="sizeFilter"
+              id="sizeFilter5"
+              value="XL"
+            />{" "}
+            XL{" "}
+          </label>{" "}
+        </div>{" "}
+      </div>{" "}
     </div>
   );
 }
